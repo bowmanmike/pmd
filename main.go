219a98fa0ec8a39a0d1397507445f78be0db7fcf
Mike Bowman <mbowman2@gmail.com>
2018-12-04 14:52:13 -0500

    Basic setup working

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gen2brain/beeep"
+)
+
+const (
+	workCycle         = 25
+	shortBreak        = 5
+	longBreak         = 20
+	cyclesBeforeBreak = 4
+)
+
+type cycleInfo struct {
+	Label    string
+	Duration int
+}
+
+func main() {
+	totalCycleCount := 0
+
+	for {
+		for i := 0; i < cyclesBeforeBreak; i++ {
+			runCycle(cycleInfo{"Work", 25})
+			if i == cyclesBeforeBreak-1 {
+				break
+			}
+			runCycle(cycleInfo{"Short Break", 5})
+		}
+		runCycle(cycleInfo{"Long Break", 20})
+
+		totalCycleCount++
+		fmt.Printf("%v full cycles completed!\n", totalCycleCount)
+	}
+}
+
+func runCycle(options cycleInfo) {
+	alertText := fmt.Sprintf("Starting %s cycle for %v minutes", options.Label, options.Duration)
+	beeep.Alert("PMD", alertText, "")
+	timer := time.NewTimer(time.Duration(options.Duration) * time.Minute)
+
+	<-timer.C
+}
